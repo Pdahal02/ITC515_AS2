7ae71281adefb1f50375466ef83aa95d360b4e82
Pdahal02 <acesencajonado@10.219.127.126>
2016-09-13 15:49:59 +1000

    *Changed naming conventions: Id to studentId, fn to firstName and ln to lastName  * Changed indentations  * Added necessary comments

--- src/datamanagement/Student.java
+++ src/datamanagement/Student.java
@@ -1,38 +1,56 @@
+/**
+ *   A student class is created which call the student object with studentID, firstName and lastName
+ */
 package datamanagement;
 
 public class Student implements IStudent {
-    private Integer id; private String fn;
-            private String ln;
-private StudentUnitRecordList su;
+	private Integer studentId; // id changed to studentID//
+	private String firstName; // fn changed to firstName//
+	private String lastName; // ln changed to lastName//
+	private StudentUnitRecordList su;
 
-public Student( Integer id, String fn, String ln, StudentUnitRecordList su ) { this.id = id; this.fn = fn;
-        this.ln = ln;this.su = 
-        su == null ? new StudentUnitRecordList() : 
-                su;
+	public Student(Integer studentstudentId, String firstName, String lastName, StudentUnitRecordList su) {
+		this.studentId = studentstudentId; // id changed to studentID//
+		this.firstName = firstName; // fn changed to firstName//
+		this.lastName = lastName; // ln changed to lastName//
+		this.su = su == null ? new StudentUnitRecordList() : su;
+	}
+
+	public Integer getID() {
+		return this.studentId; // id changed to studentID//
+	}
+
+	public String getFirstName() {
+		return firstName; // fn changed to firstName//
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName; // fn changed to firstName//
+	}
+
+	public String getLastName() {
+		return lastName; // ln changed to lastName//
+	}
+
+	public void setLastName(String lastName) {
+
+		this.lastName = lastName; // ln changed to lastName//
+	}
+
+	public void addUnitRecord(IStudentUnitRecord record) {
+		su.add(record);
+	}
+
+	public IStudentUnitRecord getUnitRecord(String unitCode) {
+		for (IStudentUnitRecord r : su)
+			if (r.getUnitCode().equals(unitCode))
+				return r;
+
+		return null;
+
+	}
+
+	public StudentUnitRecordList getUnitRecords() {
+		return su;
+	}
 }
-
-    public Integer getID() { return this.id; 
-} public String getFirstName() { 
-return fn; }
-
-    public void setFirstName( String firstName ) { 
-this.fn = firstName; }
-
-public String getLastName() { 
-    return ln; }
-    public void setLastName( String lastName ) { 
-
-        
-this.ln = lastName; }
-
-public void addUnitRecord( IStudentUnitRecord record ) { su.add(record); }
-        public IStudentUnitRecord getUnitRecord( String unitCode ) {
-for ( IStudentUnitRecord r : su ) 
-            if ( r.getUnitCode().equals(unitCode)) 
-return r; 
-
-return null;
-        
-}
-
-public StudentUnitRecordList getUnitRecords() { return su; }}
