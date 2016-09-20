d4a3322125a9820d6c3177a5e815bf3db9f1d7ed
Pdahal02 <acesencajonado@10.219.127.126>
2016-09-20 14:31:08 +1000

    Student lister Added comments Changed the layout of the program

--- src/datamanagement/IStudentLister.java
+++ src/datamanagement/IStudentLister.java
@@ -4,8 +4,8 @@ package datamanagement;
  * @author jtulip
  */
 
-public interface IStudentLister {
+public interface IStudentLister {// program gets the student list
 
-    public void clearStudents();
-    public void addStudent(IStudent student);
+    public void clearStudents();// deletes the name of the student
+    public void addStudent(IStudent student);// adds the student necessary information
 }
