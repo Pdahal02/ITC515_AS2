5480643462c90bb2bd601cc63edbea047f48084a
Pdahal02 <acesencajonado@10.219.127.126>
2016-09-20 14:39:10 +1000

    Student record list Changed the layout Added  comments

--- src/datamanagement/IUnit.java
+++ src/datamanagement/IUnit.java
@@ -2,33 +2,43 @@ package datamanagement;
 
 public interface IUnit {
 
-    public String getUnitCode();
-    public String getUnitName();
+	public String getUnitCode();// get code
 
-    public float getPsCutoff();
-    public void  setPsCutoff1(float cutoff);
+	public String getUnitName();// get name
 
-    public float getCrCutoff();
-    public void  setCrCutoff(float cutoff);
+	public float getPsCutoff();// get pass cutoff point
 
-    public float getDiCuttoff();    
-    public void  setDiCutoff(float cutoff);
+	public void setPsCutoff1(float cutoff);// set cutoff point
 
-    public float getHdCutoff();
-    public void  setHdCutoff(float cutoff);    
+	public float getCrCutoff();// get credit cutoff point
 
-    public float getAeCutoff();    
-    public void  setAeCutoff(float cutoff);
-    
-    public int getAsg1Weight();
-    public int getAsg2Weight();
-    public int getExamWeight();
-    public void setAssessmentWeights(int asg1Wgt, int asg2Wgt, int examWgt);
+	public void setCrCutoff(float cutoff);// set credit cutoff point
 
-    public String getGrade(float asg1, float asg2, float exam);
+	public float getDiCuttoff();// get distinction cutoff point
 
-    public void addStudentRecord(IStudentUnitRecord record );
-    public IStudentUnitRecord getStudentRecord(int studentID );
-    
-    public StudentUnitRecordList listStudentRecords();
+	public void setDiCutoff(float cutoff);// set distinction cutoff point
+
+	public float getHdCutoff();// get High distinction cutoff point
+
+	public void setHdCutoff(float cutoff);// set High distinction cutoff point
+
+	public float getAeCutoff();// get AE cutoff point
+
+	public void setAeCutoff(float cutoff);// set AE cutoff point
+
+	public int getAsg1Weight();// assigns weight 1
+
+	public int getAsg2Weight();// assigns weight 2
+
+	public int getExamWeight();// assigns weight 3
+
+	public void setAssessmentWeights(int asg1Wgt, int asg2Wgt, int examWgt);
+
+	public String getGrade(float asg1, float asg2, float exam);
+
+	public void addStudentRecord(IStudentUnitRecord record);
+
+	public IStudentUnitRecord getStudentRecord(int studentID);
+
+	public StudentUnitRecordList listStudentRecords();
 }
